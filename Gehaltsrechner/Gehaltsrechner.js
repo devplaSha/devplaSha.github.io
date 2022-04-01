c90647f32dbff41df86b22b578b84e2b29b6b2e1
devplaSha <102826045+devplaSha@users.noreply.github.com>
2022-04-01 23:19:54 +0200

    Add files via upload

---
+++ Gehaltsrechner/Gehaltsrechner.js
@@ -0,0 +1,31 @@
+var eins = 5000;
+var zwei = 7.500;
+var drei = 10.000;
+var vier = 12.500;
+var fünf = 15.000;
+var sechs = 17.00;
+var sieben = 20.000;
+var acht = 22.500;
+var neun = 25.000;
+var zehn = 27.500;
+var elf = 30.000;
+var zwölf = 32.500;
+var dreizehn = 35.000;
+var vierzehn = 37.500;
+var fünfzehn = 40.000;
+var sechszehn = 42.500;
+var siebzehn = 45.000;
+var achtzehn = 47.500;
+var neunzehn = 50.000;
+var zwanzig = 52.500;
+var einundzwanzig = 55.000;
+var zweiundzwanzig = 57.500;
+var dreiundzwanzig = 60.000;
+var vieundzwanzig = 62.500;
+var fünfundzwanzig = 65.000;
+
+if (document.getElementsByClassName(eingabefeld_Bruttogehalt) < (eins)){ 
+     document.getElementById(result)
+
+}
+
---
+++ ToDoList/ToDoList.js
@@ -0,0 +1,10 @@
+function addToDo(){
+    ToDoAuflistung.innerHTML = `
+    <li class="mdl-list__item">
+                  <span class="mdl-list__item-secondary-action">
+                    <label class="mdl-checkbox mdl-js-checkbox" for="list-checkbox-1">
+                      <input type="checkbox" id="list-checkbox-1" class="mdl-checkbox__input" checked />
+                    </label>
+                  </span>
+                </li>`
+}
